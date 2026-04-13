565303669091aed4234a4ae3c84487136219f677
561800-leon <561800@escg.ac.uk>
2026-04-13 15:57:06 +0100

    2

--- Assets/Main/Scripts/Weapon.cs
+++ Assets/Main/Scripts/Weapon.cs
@@ -1,35 +1,67 @@
-using JetBrains.Annotations;
 using System;
 using System.Collections;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
     public float bulletVelocity = 30f;
     public float bulletPrefabLifeTime = 3f;
 
+    public float fireRate = 0.1f;
+    private float nextFireTime = 0f;
+
+    // Shake settings
+    public float shakeAmount = 0.03f;
+    public float shakeSpeed = 20f;
+    private Vector3 originalLocalPosition;
+
+    void Start()
+    {
+        originalLocalPosition = transform.localPosition;
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        bool isFiring = Input.GetKey(KeyCode.Mouse0);
+
+        if (isFiring && Time.time >= nextFireTime)
         {
+            nextFireTime = Time.time + fireRate;
             FireWeapon();
         }
 
+        HandleShake(isFiring);
     }
+
+    void HandleShake(bool isFiring)
+    {
+        if (isFiring)
+        {
+            float x = (Mathf.PerlinNoise(Time.time * shakeSpeed, 0f) - 0.5f) * shakeAmount;
+            float y = (Mathf.PerlinNoise(0f, Time.time * shakeSpeed) - 0.5f) * shakeAmount;
+
+            transform.localPosition = originalLocalPosition + new Vector3(x, y, 0);
+        }
+        else
+        {
+            // Smoothly return to original position
+            transform.localPosition = Vector3.Lerp(transform.localPosition, originalLocalPosition, Time.deltaTime * 10f);
+        }
+    }
+
     public void FireWeapon()
     {
-        // Instantiate the bullet
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
-        // Shoot the bullet
-        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
-        // Destroy the bullet after a certain time
-        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifeTime)); 
 
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddForce(bulletSpawn.forward * bulletVelocity, ForceMode.Impulse);
+        }
 
-
+        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifeTime));
     }
 
     public IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
@@ -38,4 +70,3 @@ public class Weapon : MonoBehaviour
         Destroy(bullet);
     }
 }
-
