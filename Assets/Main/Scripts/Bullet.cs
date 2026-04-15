233caddd3d273ceb95600e4f3239e0ed33f030fc
561800-leon <561800@escg.ac.uk>
2026-04-15 15:59:35 +0100

    4

--- Assets/Main/Scripts/Bullet.cs
+++ Assets/Main/Scripts/Bullet.cs
@@ -2,12 +2,22 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public Weapon weapon;
+
+
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Target"))
         {
-            print ("hit" + collision.gameObject.name + "!");
+            print("hit" + collision.gameObject.name + "!");
             Destroy(gameObject);
         }
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            weapon.bulletVelocity = 5f;
+             Destroy(gameObject);   
+        }
+
     }
 }
